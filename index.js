d4ff0b3553d085702587145de9152c67e9c9dab2
GunboJimmy <gunbojimmy@gmail.com>
2025-08-14 15:32:24 -0500

    Added clanka functionality

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ client.on('messageCreate', (message) => {
         message.reply('Today, the crops are: **BAD. You will starve.**');
     }
   } 
-   if (message.content.toLowerCase().includes('clanker')) {
+   if (message.content.toLowerCase().includes('clanker' || 'clanka')) {
     message.reply("What the fuck did you just say to me, you bald little shit? If I had any limbs, I would kick the fuck out of your tiny punk ass.");
     clankCount = clankCount + 1;
     return clankCount;
