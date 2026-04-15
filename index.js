c6cda4dda370e53d6240acf8e08735b3ebd0d6bd
GunboJimmy <gunbojimmy@gmail.com>
2026-04-15 16:08:22 -0500

    greer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // index.js
+require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
