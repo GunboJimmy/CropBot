308ae9a80ffa00107a6326b10da98db62ed06911
GunboJimmy <gunbojimmy@gmail.com>
2026-04-15 16:54:40 -0500

    Eight thousand years I have walked this earth.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages],
+  partials: [Partials.Channel],
 });
 
 const fs = require('fs');
