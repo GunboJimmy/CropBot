34596817e63e74eeb8527f8d51111451013fa092
GunboJimmy <gunbojimmy@gmail.com>
2026-04-15 16:57:43 -0500

    I have never seen a more wretched place.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages],
   partials: [Partials.Channel],
