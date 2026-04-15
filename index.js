e670632a23465ac504a5757d59fef69240d38788
GunboJimmy <gunbojimmy@gmail.com>
2026-04-15 16:24:51 -0500

    Rest in Piss Forever Miss

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-    const command = require('./commands/${file}');
+    const command = require(path.join(commandsPath, file))
     client.commands.set(command.data.name, command);
 }
 
